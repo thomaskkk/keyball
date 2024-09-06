2588e8bd375aaf567f33f81f6bfe3fdfe5d2dace
Thomas Kuryura <thomas.adm@gmail.com>
2024-09-06 11:36:34 -0300

    Disable oled and rgb

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/rules.mk
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/rules.mk
@@ -1,4 +1,4 @@
 VIA_ENABLE       = yes
 EXTRAKEY_ENABLE  = yes   # Audio control and System control(+470)
-OLED_ENABLE      = yes
-RGBLIGHT_ENABLE  = yes
+OLED_ENABLE      = no
+RGBLIGHT_ENABLE  = no
